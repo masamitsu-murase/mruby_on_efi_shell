32d7a31bd14de8f81e79e563bd79d811e8c79eb1
Masamitsu MURASE <masamitsu.murase@gmail.com>
2012-12-10 01:26:40 +0900

    Modify array handling.

--- src/uefi_guid.c
+++ src/uefi_guid.c
@@ -1,4 +1,7 @@
 
+#include "mruby.h"
+#include "mruby/array.h"
+
 #include "uefi.h"
 
 void mrb_uefi_guid_get_guid(mrb_state *mrb, mrb_value guid, EFI_GUID *pguid)
@@ -9,7 +12,7 @@ void mrb_uefi_guid_get_guid(mrb_state *mrb, mrb_value guid, EFI_GUID *pguid)
 
     data = mrb_funcall(mrb, guid, "data", 0);
     for (i=0; i<sizeof(EFI_GUID); i++){
-        mrb_value num = mrb_funcall(mrb, data, "[]", 1, mrb_fixnum_value(i));
+        mrb_value num = RARRAY_PTR(data)[i];
         buf[i] = (UINT8)(mrb_fixnum(num));
     }
 }
