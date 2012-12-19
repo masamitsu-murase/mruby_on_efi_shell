b1491cde32bcce13dae0d094a0945f8fd1a643b5
Masamitsu MURASE <masamitsu.murase@gmail.com>
2012-12-20 02:40:53 +0900

    Change type of Guid#data.

--- mrblib/guid.rb
+++ mrblib/guid.rb
@@ -4,11 +4,10 @@ module UEFI
     attr_reader :data
 
     def initialize(arg)
-      case(arg)
-      when String
-        initialize_str(arg)
-      when Array
+      if (arg.bytesize == 16)
         initialize_raw(arg)
+      else
+        initialize_str(arg)
       end
     end
 
@@ -18,14 +17,14 @@ module UEFI
       data = str.split("-")
       raise "Invalid GUID: #{str}" if (data.size != 5)
 
-      @data = []
+      data_array = []
       [ data[0], data[1], data[2] ].each do |d|
         chars = []
         d.each_char do |ch|
           chars.push(ch)
         end
         chars.reverse.each_slice(2) do |a, b|
-          @data.push(a.to_i(16) + b.to_i(16) * 16)
+          data_array.push(a.to_i(16) + b.to_i(16) * 16)
         end
       end
       [ data[3], data[4] ].each do |d|
@@ -34,9 +33,11 @@ module UEFI
           chars.push(ch)
         end
         chars.each_slice(2) do |a, b|
-          @data.push(a.to_i(16) * 16 + b.to_i(16))
+          data_array.push(a.to_i(16) * 16 + b.to_i(16))
         end
       end
+
+      @data = data_array.pack("C*")
     end
 
     def initialize_raw(raw)
@@ -44,11 +45,11 @@ module UEFI
     end
 
     def to_s
-      data = [ @data.slice(0, 4).reverse,
-               @data.slice(4, 2).reverse,
-               @data.slice(6, 2).reverse,
-               @data.slice(8, 2),
-               @data.slice(10, 6) ]
+      data = [ @data.slice(0, 4).bytes.to_a.reverse,
+               @data.slice(4, 2).bytes.to_a.reverse,
+               @data.slice(6, 2).bytes.to_a.reverse,
+               @data.slice(8, 2).bytes.to_a,
+               @data.slice(10, 6).bytes.to_a ]
       str = data.map do |i|
         i.map{ |j| j.to_s(16).rjust(2, '0') }.join("")
       end.join("-")
--- src/uefi_guid.c
+++ src/uefi_guid.c
@@ -1,37 +1,32 @@
 
+#include <string.h>
+
 #include "mruby.h"
 #include "mruby/array.h"
+#include "mruby/string.h"
 #include "mruby/variable.h"
 
 #include "uefi.h"
 
 void mrb_uefi_guid_get_guid(mrb_state *mrb, mrb_value guid, EFI_GUID *pguid)
 {
-    UINT8 *buf = (UINT8 *)pguid;
-    int i;
     mrb_value data;
 
     data = mrb_funcall(mrb, guid, "data", 0);
-    for (i=0; i<sizeof(EFI_GUID); i++){
-        mrb_value num = RARRAY_PTR(data)[i];
-        buf[i] = (UINT8)(mrb_fixnum(num));
-    }
+    memcpy(pguid, RSTRING_PTR(data), sizeof(*pguid));
 }
 
 mrb_value mrb_uefi_guid_set_guid(mrb_state *mrb, EFI_GUID *guid)
 {
-    mrb_value array = mrb_ary_new_capa(mrb, sizeof(EFI_GUID));
+    mrb_value data = mrb_str_buf_new(mrb, sizeof(EFI_GUID));
     mrb_value uefi_ns, guid_class;
-    int i;
-    UINT8 *buf = (UINT8 *)guid;
 
-    for (i=0; i<sizeof(EFI_GUID); i++){
-        mrb_ary_push(mrb, array, mrb_fixnum_value(buf[i]));
-    }
+    mrb_str_resize(mrb, data, sizeof(EFI_GUID));
+    memcpy(RSTRING_PTR(data), guid, sizeof(*guid));
 
     uefi_ns = mrb_vm_const_get(mrb, mrb_intern(mrb, "UEFI"));
     guid_class = mrb_const_get(mrb, uefi_ns, mrb_intern(mrb, "Guid"));
 
-    return mrb_funcall(mrb, guid_class, "new", 1, array);
+    return mrb_funcall(mrb, guid_class, "new", 1, data);
 }
 
