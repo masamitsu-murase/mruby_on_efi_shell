98d646b5da24d30bb94605b13eb4a589622049cc
Masamitsu MURASE <masamitsu.murase@gmail.com>
2013-01-21 01:37:44 +0900

    Implement sample code to read disk.

--- example/read_disk.rb
+++ example/read_disk.rb
@@ -19,41 +19,51 @@ def print_binary_data(data)
 end
 
 class BlockIoProtocol < UEFI::Protocol
+  GUID = UEFI::Guid.new("964e5b21-6459-11d2-8e39-00a0c969723b")
+
   define_variable(:revision, :u64)
   define_variable(:media, :p)
   define_function(:reset, :e, [:p, :b])
   define_function(:read_blocks, :e, [:p, :u32, :u64, :u64, :p])
-  # ...
+  #...
 end
 
 # I will rename UEFI::Protocol to UEFI::Structure or any other name...
 class Media < UEFI::Protocol
   define_variable(:media_id, :uint32)
-  define_variable(:removable_media, :b)
-  define_variable(:media_present, :b)
-  define_variable(:logical_partition, :b)
-  define_variable(:read_only, :b)
-  define_variable(:write_caching, :b)
-  define_variable(:block_size, :u32)
-  define_variable(:io_align, :u32)
-  define_variable(:last_block, :u64)
+  #...
 end
 
-# EFI_BLOCK_IO_PROTOCOL_GUID
-guid = UEFI::Guid.new("964e5b21-6459-11d2-8e39-00a0c969723b")
-ptr = UEFI::BootService.locate_protocol(guid)
+# See http://wiki.phoenix.com/wiki/index.php/EFI_DISK_IO_PROTOCOL
+class DiskIoProtocol < UEFI::Protocol
+  GUID = UEFI::Guid.new("CE345171-BA0B-11d2-8e4F-00a0c969723b")
 
+  define_variable(:revision, :u64)
+  define_function(:read_disk, :efi_status, [:p, :u32, :u64, :u64, :p])
+  #...
+end
+
+
+# Find first disk.
+handles = UEFI::BootService.locate_handle_buffer(BlockIoProtocol::GUID)
+handle = handles.first
+puts "handle: #{handle}"
+
+# Locate BlockIoProtocol to get the pointer of media.
+ptr = UEFI::BootService.handle_protocol(handle, BlockIoProtocol::GUID)
 bp = BlockIoProtocol.new(ptr)
+# Get media.
 media = Media.new(bp.media)
-
-puts "revision: #{bp.revision}"
 puts "media_id: #{media.media_id}"
 
+# Find DiskIoProtocol related to bp.
+ptr = UEFI::BootService.handle_protocol(handle, DiskIoProtocol::GUID)
+dp = DiskIoProtocol.new(ptr)
+
 buf = " " * 512  # Buffer to be filled with returned data.
-st = bp.read_blocks(ptr, media.media_id, 0, buf.size, buf)
+st = dp.read_disk(ptr, media.media_id, 0, buf.size, buf)
 if (st.success?)
   print_binary_data(buf)
 else
   puts "ERROR: #{st}"
 end
-
